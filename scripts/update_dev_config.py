c2934e9c453d2e16e64e3d950c38a9551ece1a94
John Gomersall <thegoms@gmail.com>
2026-03-23 17:31:15 +0000

    Added comments to the script
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- scripts/update_dev_config.py
+++ scripts/update_dev_config.py
@@ -23,8 +23,14 @@ from ruyaml import YAML
 import shutil
 import pathlib
 
-pathlib.Path('configs/exporter_exporter/dev').mkdir(parents=True, exist_ok=True) 
-shutil.copytree("configs/prometheus", "configs/prometheus-dev", dirs_exist_ok=True)
+PROMETHEUS_CONFIG = "configs/prometheus"
+PROMETHEUS_DEV_CONFIG = "configs/prometheus-dev"
+EXPORTER_EXPORTER_CONFIG = 'configs/exporter_exporter'
+EXPORTER_EXPORTER_DEV_CONFIG = f"{EXPORTER_EXPORTER_CONFIG}/dev"
+EXPORTER_EXPORTER_DEV_URL = "http://host.docker.internal:8202/"
+
+pathlib.Path(EXPORTER_EXPORTER_DEV_CONFIG).mkdir(parents=True, exist_ok=True) 
+shutil.copytree(PROMETHEUS_CONFIG, PROMETHEUS_DEV_CONFIG, dirs_exist_ok=True)
 
 # Note that comments aren't always removed cleanly as they can sometimes get attached
 # to the wrong node. Could use typ="safe" here to not load any comments 
@@ -32,37 +38,43 @@ shutil.copytree("configs/prometheus", "configs/prometheus-dev", dirs_exist_ok=Tr
 yaml = YAML()
 yaml.indent(mapping=2, sequence=4, offset=2)
 
-with open("configs/prometheus/config.yml", "r", encoding="utf-8") as f:
+with open(f"{PROMETHEUS_CONFIG}/config.yml", "r", encoding="utf-8") as f:
     prometheus_config = yaml.load(f)
 
 for key, value in prometheus_config.items():
     if key == "scrape_configs":
         for j in range(len(value) - 1, -1, -1):
             scrape_config = value[j]
+            # Remove config for legacy infrastructure that we can't test in dev
             if scrape_config["job_name"] in ["free-exporters", "ks1-exporters"]:
                 value.pop(j)
+
             elif "scheme" in scrape_config:
                 # Assume it is an exporter_exporter
                 scrape_config.pop("scheme") # Revert to default of http
-                scrape_config.pop("basic_auth", None)
+                scrape_config.pop("basic_auth", None) # No authentication for local connection
                 static_configs = scrape_config.get("static_configs", [])
                 for s in range(len(static_configs) - 1, -1, -1):
                     server = static_configs[s]
                     labels = server["labels"]
+                    # Don't include staging exporters as we only emulate production locally
                     if labels["env"] == "staging":
                         static_configs.pop(s)
                     else:
                         server_name = labels["server"]
+                        # Create a dev exporter_exporter config for each target referenced for the server
                         for target in server["targets"]:
-                            config_name = f"configs/exporter_exporter/{server_name}/{target.split('/')[0]}.yaml"
+                            config_name = f"{EXPORTER_EXPORTER_CONFIG}/{server_name}/{target.split('/')[0]}.yaml"
                             if os.path.exists(config_name):
-                                shutil.copy(config_name, "configs/exporter_exporter/dev")
+                                shutil.copy(config_name, EXPORTER_EXPORTER_DEV_CONFIG)
                             else:
                                 print(f"*** exporter_exporter config not found for {server_name}: {target}", file=sys.stderr)
-                            
-                        labels["base_url"] = "http://host.docker.internal:8202/"
+
+                        # Point all exporter_exporter configs to the local URL
+                        labels["base_url"] = EXPORTER_EXPORTER_DEV_URL
+
                         # Not sure whether to do this as may affect dashboards
                         # labels["env"] = "localhost"
     
-with open("configs/prometheus-dev/config.yml", "w", encoding="utf-8") as f:
+with open(f"{PROMETHEUS_DEV_CONFIG}/config.yml", "w", encoding="utf-8") as f:
     yaml.dump(prometheus_config, f)
